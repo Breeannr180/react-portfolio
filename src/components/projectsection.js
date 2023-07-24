809c62ae6ec3c48b3d788d403d9dea367f1ed30c
BreeAnn <breeannbond@gmail.com>
2023-07-24 14:11:40 -0700

    added additional work projects/links

--- src/components/projectsection.js
+++ src/components/projectsection.js
@@ -5,12 +5,13 @@ const ProjectSection = () => {
   return (
     <section id="work">
       <h2>Work</h2>
-      <Project imageUrl="./images/Koding Kitchen.png" altText="Recipe Generator" linkUrl="https://example.com/project1" projectTitle="Recipe Generator" />
-      <Project imageUrl="./images/Weather App.png" altText="Weather App" linkUrl="https://example.com/project2" projectTitle="Weather App" />
-      <Project imageUrl="./images/workday scheduler.png" altText="Work Item 3" linkUrl="https://example.com/project3" projectTitle="Work Item 3" />
-      <Project imageUrl="./images/Horiseon code refractor.png" altText="Work Item 4" linkUrl="https://example.com/project4" projectTitle="Work Item 4" />
-      <Project imageUrl="./images/passwordgenerator.png" altText="Work Item 5" linkUrl="https://example.com/project5" projectTitle="Work Item 5" />
-      <Project imageUrl="./images/passwordgenerator.png" altText="Work Item 5" linkUrl="https://example.com/project5" projectTitle="Work Item 5" />
+      <Project imageUrl="./images/Koding Kitchen.png" altText="Recipe Generator" linkUrl="https://ggdave.github.io/Koding-Kitchen/" projectTitle="Recipe Generator" />
+      <Project imageUrl="./images/Weather App.png" altText="Weather App" linkUrl="https://breeannr180.github.io/weather-app/" projectTitle="Weather App" />
+      <Project imageUrl="./images/workday scheduler.png" altText="Work Day Scheduler" linkUrl="https://breeannr180.github.io/work-day-scheduler/" projectTitle="Work Day Scheduler" />
+      <Project imageUrl="./images/Horiseon code refractor.png" altText="Work Item 4" linkUrl="https://breeannr180.github.io/code-refactor/" projectTitle="Code Refactor" />
+      <Project imageUrl="./images/passwordgenerator.png" altText="Work Item 5" linkUrl="https://breeannr180.github.io/javascript-password-create/" projectTitle="Password Generator" />
+      <Project imageUrl="./images/Coding Quiz.png" altText="Coding Quiz" linkUrl="https://breeannr180.github.io/coding-quiz/" projectTitle="Coding Quiz" />
+      <Project imageUrl="./images/Whiskers and Wags.png" altText="Pet Adoption Agency" linkUrl="https://guarded-oasis-57262-32f6a86c3b9b.herokuapp.com/" projectTitle="Pet Adoption Agency" />
     </section>
   );
 };
