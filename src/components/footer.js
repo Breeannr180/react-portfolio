60c0601c175115853098a102fa2d47151f18f526
BreeAnn <breeannbond@gmail.com>
2023-07-24 21:28:10 -0700

    updated header and footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer">
-      <h4>BreeAnn 2023!</h4>
+      <h4>BreeAnn Bond 2023!</h4>
     </footer>
   );
 };
