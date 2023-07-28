3723a3b9c1c53ca14299938812341dcb509f8e6c
BreeAnn <breeannbond@gmail.com>
2023-07-27 19:12:05 -0700

    fixed bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'reac
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
